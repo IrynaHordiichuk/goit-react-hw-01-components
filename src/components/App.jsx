5ff2f290b1a4dd9883b7bc7b1e8109d24109aaf7
Iryna Hordiichuk <72663259+IrynaHordiichuk@users.noreply.github.com>
2022-11-14 20:15:28 +0000

    task one

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+
+import { Heading, Section, Container, Profile, FriendList, Statistics, TransactionHistory } from 'components';
+
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
+import user from 'data/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+
+    <Section>
+      <Container>
+      {/* <Heading marginBottom="50px" textAlign="center">
+          Task 1
+        </Heading> */}
+       <Profile {...user} />
+       <Statistics title={"kgkgkhgkg"} stats={data} />
+      </Container>
+    </Section>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import 'modern-normalize';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { App } from 'components';
+import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
