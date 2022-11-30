6c9e398d1bf44755114d15c196e7160436cc99a9
Iryna Hordiichuk <72663259+IrynaHordiichuk@users.noreply.github.com>
2022-11-30 21:25:03 +0000

    update

--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -7,7 +7,6 @@ export const StatisticsSection = styled.section`
   background: ${props => props.theme.colors.white};
   margin: 50px auto 0;
   width: 700px;
-
 `;
 
 export const Title = styled.h2`
@@ -15,33 +14,32 @@ export const Title = styled.h2`
   color: ${props => props.theme.colors.gray};
   text-align: center;
   text-transform: capitalize;
-  margin-bottom: 0; 
+  margin-bottom: 0;
   padding: ${props => props.theme.spacing(8)};
-
 `;
 
 export const StatList = styled.ul`
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const StatItem = styled.li`
   display: flex;
-  align-items: center;
-  padding: 30px;
+  flex-direction: column;
+  padding: ${props => props.theme.spacing(5)};
+  gap: ${props => props.theme.spacing(2)};
   flex-basis: calc((100% - 4 * 0px) / 5);
-  background: ${ function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}}
+  background: ${function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }};
 `;
 
 export const Label = styled.span`
-    color: ${props => props.theme.colors.white};
+  color: ${props => props.theme.colors.white};
 `;
 
 export const Percentage = styled.span`
-  font-size: 20px;
+font-size: ${props => props.theme.fontSizes.large};
   color: ${props => props.theme.colors.white};
-`
-
+`;
